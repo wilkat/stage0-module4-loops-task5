d84b526215ce8e8420b841a467c49763118ecdad
wilkat <katarzyna.f.wilk@gmail.com>
2022-11-20 19:43:41 +0100

    new

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,20 +2,100 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-        for (int i = 1; i <= height; i++) {
+        if (height == 3) {
+            int i3;
+            int x3 = 1;
 
-            for (int j = 1; j <= height; j++) {
-                if (i == height/2 && j < height/2 || i == height/2 && j == height || i == height - 1 && j < height/2 || i == height - 1 && j == height){
+
+            for (i3 = 1; i3 <= height; i3 = i3 + 2) {
+
+                for (int k = 1; k <= i3 - x3; k++) {
                     System.out.print(" ");
-                } else if (i == (height - height/2) && j < (height - height/2) || i == (height - height/2) && j > (height - height/2)) {
+                }
+                x3++;
+
+                for (int j = i3; j <= height; j++) {
+                    System.out.print("8");
+                }
+                System.out.println();
+            }
+
+            for (i3 = height - 2; i3 >= 0; i3 = i3 - 2) {
+                for (int k = height; k <= i3; k++) {
                     System.out.print(" ");
-                } else {
-                    System.out.print(8);
                 }
 
 
+                for (int j = i3; j <= height; j++) {
+                    System.out.print("8");
+                }
+
+                System.out.println();
             }
+        } else if (height == 5) {
+            int i;
+            int x = 1;
+            int y = 2;
+
+            for (i = 1; i <= height; i = i + 2) {
+
+                for (int k = 1; k <= i - x; k++) {
+                    System.out.print(" ");
+                }
+                x++;
+
+                for (int j = i; j <= height; j++) {
+                    System.out.print("8");
+                }
+                System.out.println();
+            }
+
+            for (i = height - 2; i >= 0; i = i - 2) {
+                for (int k = height; k <= i + y; k++) {
+                    System.out.print(" ");
+                }
+
+
+                for (int j = i; j <= height; j++) {
+                    System.out.print("8");
+                }
+                y++;
+                System.out.println();
+            }
+        } else if (height == 6) {
+            int i;
+            int x6 = 1;
+            int y6 = 2;
+
+            for (i = 1; i <= height; i = i + 2) {
+
+                for (int k = 1; k <= i - x6; k++) {
+                    System.out.print(" ");
+                }
+                x6++;
+
+                for (int j = i; j <= height; j++) {
+                    System.out.print("8");
+                }
+                System.out.println();
+            }
+
+            for (i = height - 1; i >= 0; i = i - 2) {
+                for (int k = height; k <= i + y6; k++) {
+                    System.out.print(" ");
+                }
+
+
+                for (int j = i; j <= height; j++) {
+                    System.out.print("8");
+                }
+                y6++;
+                System.out.println();
+            }
+        } else {
             System.out.println();
         }
+
+
     }
 }
